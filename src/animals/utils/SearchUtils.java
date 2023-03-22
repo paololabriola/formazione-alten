695144b6c0c0f0c2fd5b5483d23940360ba97c5d
paololabriola <paololabriola02@gmail.com>
2023-03-22 11:40:17 +0100

    Fix class name searchUtils with CapitalCase

--- src/animals/utils/SearchUtils.java
+++ src/animals/utils/SearchUtils.java
@@ -1,51 +1,49 @@
 package animals.utils;
 
-import animals.generals.Animal;
-import animals.generals.Tailed;
-import animals.generals.Winged;
+import animals.model.abstractation.Animal;
+import animals.model.abstractation.Tailed;
+import animals.model.abstractation.Winged;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class searchUtils {
-    //Searching method for the tallest and the shortest elements of an animals' list.
+public class SearchUtils {
+
     public static List<Animal> getTallestAndShortest(List<Animal> animals){
 
-        //Declaring tallest and shortest animal list.
         List<Animal> tallestAndShortest = new ArrayList<>();
-        //Applying the sorting method defined in the sortUtils class onto animals list taken as input.
-        sortUtils.sortByHeight(animals);
+        SortUtils.sortByHeight(animals);
+
         //After sorting the list we are going to assign the first and last element to the tallestAndShortest list in order to save the tallest and the shortest elements.
+
         //Shortest (first element of the list, which is in position 0)
         tallestAndShortest.add(animals.get(0));
         //Tallest (max size of the list minus 1, cus its position counter starts from 0)
         tallestAndShortest.add(animals.get(animals.size() - 1));
-        //Then we return the list which contains only the tallest and the shortest elements.
+
         return tallestAndShortest;
 
     }
 
-    //Searching method for the heaviest and the lightest elements of an animals' list.
     public static List<Animal> getHeaviestAndLightest(List<Animal> animals){
 
-        //Declaring heaviest and lightest animal list.
         List<Animal> heaviestAndLightest = new ArrayList<>();
-        //Applying the sorting method defined in the sortUtils class onto animals list taken as input.
-        sortUtils.sortByWeight(animals);
+        SortUtils.sortByWeight(animals);
+
         //After sorting the list we are going to assign the first and last element to the heaviestAndLightest list in order to save the heaviest and the lightest elements.
+
         //Lightest (first element of the list, which is in position 0)
         heaviestAndLightest.add(animals.get(0));
         //Heaviest (max size of the list minus 1, cus its position counter starts from 0)
         heaviestAndLightest.add(animals.get(animals.size() - 1));
-        //Then we return the list which contains only the heaviest and the lightest elements.
+
         return heaviestAndLightest;
 
     }
 
-    //Searching method for the longestTailed element of an animals' list.
     public static Tailed getLongestTailElement(List<Tailed> animals){
 
-        Tailed longestTailElement = sortUtils.sortByTailLength(animals).get(animals.size() - 1);
+        Tailed longestTailElement = SortUtils.sortByTailLength(animals).get(animals.size() - 1);
 
         return longestTailElement;
 
@@ -53,7 +51,7 @@ public class searchUtils {
 
     public static Winged getBiggestWingSpanElement(List<Winged> animals){
 
-        Winged biggestWingSpanElement = sortUtils.sortByWingSpan(animals).get(animals.size() - 1);
+        Winged biggestWingSpanElement = SortUtils.sortByWingSpan(animals).get(animals.size() - 1);
 
         return biggestWingSpanElement;
 
