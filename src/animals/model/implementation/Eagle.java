2496f8781cbfbe1a59355dfb2af60736f8159515
paololabriola <paololabriola02@gmail.com>
2023-03-22 11:45:19 +0100

    Change class path to model/implementation

--- src/animals/model/implementation/Eagle.java
+++ src/animals/model/implementation/Eagle.java
@@ -1,9 +1,10 @@
-package animals.species;
+package animals.model.implementation;
 
-import animals.generals.Winged;
+import animals.model.abstractation.Winged;
 
 public class Eagle extends Winged {
 
+    //Class constructor
     public Eagle(String nameOfTheSpecimen, String favouriteFood, int ageOfTheSpecimen, double weightOfTheSpecimen, double heightOfTheSpecimen, double wingSpan) {
 
         super(nameOfTheSpecimen, favouriteFood, ageOfTheSpecimen, weightOfTheSpecimen, heightOfTheSpecimen, wingSpan);
--- src/animals/model/implementation/Lion.java
+++ src/animals/model/implementation/Lion.java
@@ -1,9 +1,10 @@
-package animals.species;
+package animals.model.implementation;
 
-import animals.generals.Tailed;
+import animals.model.abstractation.Tailed;
 
 public class Lion extends Tailed {
 
+    //Class constructor
     public Lion(String nameOfTheSpecimen, String favouriteFood, int ageOfTheSpecimen, double weightOfTheSpecimen, double heightOfTheSpecimen, double tailLenght) {
 
         super(nameOfTheSpecimen, favouriteFood, ageOfTheSpecimen, weightOfTheSpecimen, heightOfTheSpecimen, tailLenght);
